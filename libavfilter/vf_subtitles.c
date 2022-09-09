f00d56e78208d749e80f7ebe6d873f00890a4925
wangzh <wangzhenghang@1sj.tv>
2022-09-09 17:58:13 +0800

    FIX：修复字幕结束会多一帧的bug

--- libavfilter/vf_subtitles.c
+++ libavfilter/vf_subtitles.c
@@ -184,16 +184,17 @@ static void overlay_ass_image(AssContext *ass, AVFrame *picref,
 
 static int filter_frame(AVFilterLink *inlink, AVFrame *picref)
 {
+    if (picref->pts == AV_NOPTS_VALUE) return 0;
     AVFilterContext *ctx = inlink->dst;
     AVFilterLink *outlink = ctx->outputs[0];
     AssContext *ass = ctx->priv;
     int detect_change = 0;
-    double time_ms = picref->pts * av_q2d(inlink->time_base) * 1000;
+    long long time_ms = llrint(picref->pts * av_q2d(inlink->time_base) * 1000);
     ASS_Image *image = ass_render_frame(ass->renderer, ass->track,
                                         time_ms, &detect_change);
 
     if (detect_change)
-        av_log(ctx, AV_LOG_DEBUG, "Change happened at time ms:%f\n", time_ms);
+        av_log(ctx, AV_LOG_DEBUG, "Change happened at time ms:%lld\n", time_ms);
 
     overlay_ass_image(ass, picref, image);
 
