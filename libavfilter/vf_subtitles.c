622b2cbc5f2dd6a1a450a30a7d1c8f9b4b55e130
passerbya <hanghang3103@163.com>
2023-03-02 17:35:33 +0800

    REMOVE：多余的ass_set_storage_size

--- libavfilter/vf_subtitles.c
+++ libavfilter/vf_subtitles.c
@@ -146,7 +146,6 @@ static int config_input(AVFilterLink *inlink)
 
     ff_draw_init(&ass->draw, inlink->format, ass->alpha ? FF_DRAW_PROCESS_ALPHA : 0);
 
-    ass_set_storage_size (ass->renderer, inlink->w, inlink->h);
     ass_set_frame_size  (ass->renderer, inlink->w, inlink->h);
     if (ass->original_w && ass->original_h) {
         ass_set_pixel_aspect(ass->renderer, (double)inlink->w / inlink->h /
